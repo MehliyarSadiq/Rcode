a64f6d4644fc97e4dfbe0a067eb47173d8cb081f
Mehliyar <mehliyarsadiq@Mehliyars-Mac-mini.local>
2017-03-28 17:47:48 +0800

    algorithm change
    show positive or negative relationship alone

--- function.R
+++ function.R
@@ -1,18 +1,23 @@
 # read in two maps with changes in two variables, e.g. O3_SRF and DV_O3
 # if the changes have the same sign, output 1, else -1
 source("~/Dropbox/Projects/ozone_vegetation/R/functions_Amos/get_geo.R")
-two_changes_sign = function(var1, var2, lat, lon)
+two_changes_sign = function(var1, var2, type, lat, lon)
 {
-  # set positive changes to be 1, negative to be -1
-  indx = which(var1 > 0, arr.ind = TRUE)
-  var1[indx] = 1
-  indx = which(var1 < 0, arr.ind = TRUE)
-  var1[indx] = -1
+  result = array(NaN, dim(var1))
   
-  indx = which(var2 > 0, arr.ind = TRUE)
-  var2[indx] = 1
-  indx = which(var2 < 0, arr.ind = TRUE)
-  var2[indx] = -1
+  multi = var1 * var2
+  
+  if(type == "positive") 
+  {
+    indx = which(multi > 0, arr.ind = TRUE)
+    result[indx] = 1
+  }
+  
+  if(type == "negative")
+  {
+    indx = which(multi < 0, arr.ind = TRUE)
+    result[indx] = -1
+  }
   # multiply them together and plot it
-  plot.field(var1*var2, lon, lat, type = "sign", zlim = c(-2,2), Pacific.centric = TRUE)
+  plot.field(result, lon, lat, type = "sign", zlim = c(-2,2), Pacific.centric = TRUE)
 }
