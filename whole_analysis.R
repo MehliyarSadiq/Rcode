549d70c1bcbd558b8add8c39149d6144712659f7
Mehliyar <mehliyarsadiq@Mehliyars-Mac-mini.local>
2017-04-07 16:35:18 +0800

    polish

--- whole_analysis.R
+++ whole_analysis.R
@@ -5,20 +5,21 @@ source("~/Dropbox/Projects/ozone_vegetation/R/data_extract/1.2.2_Aves/fmoz_clm45
 setwd("/Users/mehliyarsadiq/Dropbox/Projects/ozone_vegetation/R/data_extract/1.2.2_Aves/fmoz_clm45/difnc")
 
 # analyze sign relationship between two given variable changes
-
-#var.given = c("QVEGT","RS", "negative")
+# choose one pair of variables below to analyse
+# var.given = c("QVEGT","RS", "negative")
 # var.given = c("DV_O3","RS", "negative")
 # var.given = c("ELAI","PSN", "positive")
 # var.given = c("ELAI","DV_O3", "positive")
 # var.given = c("O3_SRF","ISOP_SRF", "positive")
 # var.given = c("O3_SRF","QVEGT", "negative")
-# var.given = c("O3_SRF","DV_O3", "negative")
+var.given = c("O3_SRF","DV_O3", "negative")
 # var.given = c("RH","QVEGT", "positive")
 # var.given = c("PRECT","QVEGT", "positive")
 
 # available names for var.given
-var.list = c("BTRAN", "ELAI", "H2OSOI", "LHFLX","PBLH","PRECT","RH","QBOT","QVEGT","DV_O3", "ISOP_SRF", "O3_SRF","PSN","RS")
+# var.list = c("BTRAN", "ELAI", "H2OSOI", "LHFLX","PBLH","PRECT","RH","QBOT","QVEGT","DV_O3", "ISOP_SRF", "O3_SRF","PSN","RS")
 
+# read in changes in two variables
 filename.tmp = paste0(var.given[1], "_dan-ctr.nc")
 file.tmp = open.ncdf(filename.tmp, write = FALSE)
 var.value1 = get.var.ncdf(file.tmp, var.given[1])
@@ -30,7 +31,7 @@ filename.tmp = paste0(var.given[2], "_dan-ctr.nc")
 file.tmp = open.ncdf(filename.tmp, write = FALSE)
 var.value2 = get.var.ncdf(file.tmp, var.given[2])
 close.ncdf(file.tmp)
-
+# use the function
 sign_harmony = two_changes_sign(var.value1, var.value2, type=var.given[3], lat, lon)
 
 
